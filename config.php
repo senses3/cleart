b65ea40d27225c2b596b7d83c8d71c475feba34d
senses3 <senses3@gmail.com>
2015-10-22 07:06:58 -0400

    crappy website
    how do i figure out where the contact info is in the database?

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://clevelandart.com/');
+define('HTTP_IMAGE', 'http://clevelandart.com/image/');
+define('HTTP_ADMIN', 'http://clevelandart.com/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://clevelandart.com/');
+define('HTTPS_IMAGE', 'http://clevelandart.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home4/clevela5/public_html/catalog/');
+define('DIR_SYSTEM', '/home4/clevela5/public_html/system/');
+define('DIR_DATABASE', '/home4/clevela5/public_html/system/database/');
+define('DIR_LANGUAGE', '/home4/clevela5/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home4/clevela5/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home4/clevela5/public_html/system/config/');
+define('DIR_IMAGE', '/home4/clevela5/public_html/image/');
+define('DIR_CACHE', '/home4/clevela5/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home4/clevela5/public_html/download/');
+define('DIR_LOGS', '/home4/clevela5/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'clevela5_oc333');
+define('DB_PASSWORD', '*****');
+define('DB_DATABASE', 'clevela5_oc333');
+define('DB_PREFIX', '');
+?>
